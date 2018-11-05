907b37500a8ba0dbab2eab4e18a389dba23e6d0d
Brynn Shepherd <brynn.shepherd@gmail.com>
2018-11-05 16:30:59 -0500

    more work on merge sort

--- mergesort.js
+++ mergesort.js
@@ -10,20 +10,49 @@ function merge(left, right) {
   let lPointer = 0;
   let rPointer = 0;
   let mergedArray = [];
-  while (mergedArray.length !== left.length + right.length) {
-    if (!left[lPointer]) {
-      mergedArray = mergedArray.concat(right.slice(rPointer));
-    }
-    if (!right[rPointer]) {
-      mergedArray = mergedArray.concat(left.slice(lPointer));
-    }
+  while (lPointer < left.length && rPointer < right.length) {
     if (left[lPointer] < right[rPointer]) {
       mergedArray.push(left[lPointer]);
       lPointer++;
-    } else if (right[rPointer] < left[lPointer]) {
+    } else {
       mergedArray.push(right[rPointer]);
       rPointer++;
     }
   }
+  // add on the rest of one array if it's bigger
+  if (rPointer < right.length) {
+    for (rPointer; rPointer < right.length; rPointer++) {
+      mergedArray.push(right[rPointer]);
+    }
+  }
+
+  if (lPointer < left.length) {
+    for (lPointer; lPointer < left.length; lPointer++) {
+      mergedArray.push(left[lPointer]);
+    }
+  }
+
+  // if (!left[lPointer]) {
+  //   mergedArray = mergedArray.concat(right.slice(rPointer));
+  //   break;
+  // }
+  // if (!right[rPointer]) {
+  //   mergedArray = mergedArray.concat(left.slice(lPointer));
+  //   break;
+  // }
   return mergedArray;
 }
+
+function mergeSort(array) {
+  if (array.length === 1) {
+    return array;
+  } else {
+    debugger;
+    let returnedArray = split(array);
+    let left = returnedArray[0];
+    let right = returnedArray[1];
+    return merge(mergeSort(left), mergeSort(right));
+  }
+}
+
+mergeSort([4, 5, 3, 1, 2]);
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -7,5 +7,13 @@ describe('Split Array function', function() {
 describe('Merge function', function() {
   it('is able to merge two sorted arrays into one sorted array', function() {
     expect(merge([1, 5, 8], [2, 6])).toEqual([1, 2, 5, 6, 8]);
+    expect(merge([3, 7, 23, 42], [2, 5, 9])).toEqual([2, 3, 5, 7, 9, 23, 42]);
+  });
+});
+
+describe('Merge sort function', function() {
+  it('sorts an array using merge sort', function() {
+    console.log(typeof toEqual());
+    expect(mergeSort([4, 5, 3, 1, 2]).toEqual([1, 2, 3, 4, 5]));
   });
 });
